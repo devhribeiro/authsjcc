f79069fbc7945c9da4079732bf919cb3c29f7a4d
Mário Valney <mariovalney@gmail.com>
2020-01-07 16:20:51 -0300

    It's alive

---
+++ src/models/SJCC_Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class SJCC_Login extends Component {
+  render() {
+    return (
+      <Text>Hello World</Text>
+    )
+  }
+};
+
+export default SJCC_Login;
